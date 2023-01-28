7a0b6d7b69a80862beb89e75157a22fb28cacd47
Gabrielle <gabrielle01.spence@gmail.com>
2023-01-27 23:18:07 -0800

    implement put and delete routes

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -6,7 +6,7 @@ function edit_form (data){
         <Default>
             <main>
                 <h1>Edit Place</h1>
-                <form method="POST" action={`/places/${data.id}?_method=PUT`}>
+                <form method="POST" action={`/places/${data.place.id}?_method=PUT`}>
                     <div className='row'>
                         <div className='form-group col-sm'>
                             <label htmlFor='name'>Place Name</label>
@@ -34,7 +34,11 @@ function edit_form (data){
                         <label htmlFor='cuisines'>Cuisines</label>
                         <input className ="form-control" id='cuisines' name='cuisines' required/>
                     </div>
-                    <input className = "btn btn-primary" type='submit' value='Edit' />
+                    <div className='form-group'>
+                        <label htmlFor='founded'>Founded</label>
+                        <input className ="form-control" id='founded' name='founded' defaultValue={data.place.founded}/>
+                    </div>
+                    <input className = "btn btn-primary" type='submit' defaultValue='Update' />
                 </form>
             </main>
         </Default>
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -57,8 +57,8 @@ function show (data) {
                         <p>Serving {data.place.cuisines}</p>
                     </div>
                 </div>
-                <a href={`/places/${data.id}/edit`} className="btn btn-warning">Edit</a>
-                <form method = "POST" action={`/places/${data.id}?_method=DELETE`}>
+                <a href={`/places/${data.place.id}/edit`} className="btn btn-warning">Edit</a>
+                <form method = "POST" action={`/places/${data.place.id}?_method=DELETE`}>
                     <button type='submit' className='btn btn-danger'>Delete</button>
                 </form>
                 <hr></hr>
